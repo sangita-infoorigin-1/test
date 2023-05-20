c899844b646f74feed404ab20c473ca2de39da41
sangita-infoorigin-1 <72428347+sangita-infoorigin-1@users.noreply.github.com>
2023-05-20 09:18:27 +0530

    test code

---
+++ test.py
@@ -0,0 +1,18 @@
+class test:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+   
+    def display(self):
+        print(self.x)
+        print(self.y)
+        
+
+    def sum(self):
+        z = self.x + self.y   
+        print('sum=', z) 
+        
+def show(x):
+    print(x)
+
+
